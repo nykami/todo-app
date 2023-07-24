2f423b512ef9c99fc77abbc4c43c139a34e501c0
nykami <kamilla.nyitrai@yahoo.com>
2023-07-24 16:12:27 +0300

    ITL-9 Add an add button,  include the icon

--- todo-app/src/components/TodoHeader.vue
+++ todo-app/src/components/TodoHeader.vue
@@ -1,7 +1,25 @@
 <template>
-    <h2 class="text-black text-7xl font-semibold">To do list</h2>
+  <div class="left-[415px] top-[170.93px] relative">
+    <div class="left-0 top-0 absolute text-black text-7xl font-semibold">
+      To do list
+    </div>
+    <div class="w-[59.64px] h-[59.64px] left-[550.36px] top-[13.18px] absolute">
+      <button
+        class="w-[59.64px] h-[59.64px] left-0 top-0 absolute bg-emerald-400 rounded-full"
+      >
+          <svg
+            class="w-7 h-7 left-[15.82px] top-[15.82px] absolute"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M15.0013 5.08246H13.3607C13.2149 5.08246 13.142 5.15538 13.142 5.30121V13.8871H4.99353C4.8477 13.8871 4.77478 13.9601 4.77478 14.1059V15.7465C4.77478 15.8924 4.8477 15.9653 4.99353 15.9653H13.142V24.5512C13.142 24.697 13.2149 24.77 13.3607 24.77H15.0013C15.1472 24.77 15.2201 24.697 15.2201 24.5512V15.9653H23.3685C23.5144 15.9653 23.5873 15.8924 23.5873 15.7465V14.1059C23.5873 13.9601 23.5144 13.8871 23.3685 13.8871H15.2201V5.30121C15.2201 5.15538 15.1472 5.08246 15.0013 5.08246Z"
+              fill="white"
+            />
+          </svg>
+      </button>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>

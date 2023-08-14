33dd2104fe3e4479c32f1a962cda1fe9e1ea1c6e
nykami <kamilla.nyitrai@yahoo.com>
2023-08-14 12:17:31 +0300

    ITL-8.5 Make app mobile-friendly

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,238 +1,235 @@
 <template>
-    <div class="flex flex-col items-center justify-center font-custom">
-      <div class="max-w-screen-sm">
-        <TodoLogin path="/login" button-name="Log out"/>
-        <TodoHeader @addTodo="addTodo" />
-        <SearchBar v-if="todos.length" @filterTodos="filterTodos" />
-        <Sorting
-          v-if="todos.length"
-          :sortType="sortType"
-          :sortByField="sortByField"
-          @handleSort="applySortBy"
-          @toggleSortType="changeSortType"
-        />
-        <TodoPlaceholder v-if="!todos.length" />
-        <TodoList
-          v-else
-          :reversedTodos="reversedTodos"
-          @deleteTodo="deleteTodo"
-          @updateTodo="updateTodo"
-          @setEditState="setEditState"
-          @handleCheckboxClick="handleCheckboxClick"
-        />
-        <div v-if="!filteredTodos.length && todos.length" class="text-center">
-          No todos found
-        </div>
-      </div>
+  <div class="flex flex-col items-center justify-center font-custom">
+    <TodoLogin path="/login" button-name="Log out" />
+    <TodoHeader @addTodo="addTodo" />
+    <SearchBar v-if="todos.length" @filterTodos="filterTodos" />
+    <Sorting
+      v-if="todos.length"
+      :sortType="sortType"
+      :sortByField="sortByField"
+      @handleSort="applySortBy"
+      @toggleSortType="changeSortType"
+    />
+    <TodoPlaceholder v-if="!todos.length" />
+    <TodoList
+      v-else
+      :reversedTodos="reversedTodos"
+      @deleteTodo="deleteTodo"
+      @updateTodo="updateTodo"
+      @setEditState="setEditState"
+      @handleCheckboxClick="handleCheckboxClick"
+    />
+    <div v-if="!filteredTodos.length && todos.length" class="text-center">
+      No todos found
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { computed, ref } from 'vue';
-  import TodoLogin from '../components/header/TodoLogin.vue';
-  import TodoHeader from '../components/header/TodoHeader.vue';
-  import TodoPlaceholder from '../components/TodoPlaceholder.vue';
-  import TodoList from '../components/TodoList.vue';
-  import SearchBar from '../components/SearchBar.vue';
-  import Sorting from '../components/Sorting.vue';
-  import { Todo } from '../components/types/Todo.vue';
-  
-  const todos = ref<Todo[]>([]);
-  const searchText = ref<string>('');
-  const sortType = ref<string>('desc');
-  const sortByField = ref<string>('');
-  const isSortingApplied = ref(false);
-  
-  const filteredTodos = computed(() => {
-    if (!searchText.value) {
-      return todos.value;
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import TodoLogin from '../components/header/TodoLogin.vue';
+import TodoHeader from '../components/header/TodoHeader.vue';
+import TodoPlaceholder from '../components/TodoPlaceholder.vue';
+import TodoList from '../components/TodoList.vue';
+import SearchBar from '../components/SearchBar.vue';
+import Sorting from '../components/Sorting.vue';
+import { Todo } from '../components/types/Todo.vue';
+
+const todos = ref<Todo[]>([]);
+const searchText = ref<string>('');
+const sortType = ref<string>('desc');
+const sortByField = ref<string>('');
+const isSortingApplied = ref(false);
+
+const filteredTodos = computed(() => {
+  if (!searchText.value) {
+    return todos.value;
+  }
+  return todos.value.filter(
+    (todo) =>
+      todo.title.toLowerCase().includes(searchText.value) ||
+      todo.content.toLowerCase().includes(searchText.value),
+  );
+});
+
+const reversedTodos = computed(() => {
+  return filteredTodos.value.slice().reverse();
+});
+
+function findNextId(): number {
+  let maxId = -1;
+  todos.value.forEach((todo) => {
+    if (todo.id > maxId) {
+      maxId = todo.id;
     }
-    return todos.value.filter(
-      (todo) =>
-        todo.title.toLowerCase().includes(searchText.value) ||
-        todo.content.toLowerCase().includes(searchText.value),
-    );
   });
-  
-  const reversedTodos = computed(() => {
-    return filteredTodos.value.slice().reverse();
-  });
-  
-  function findNextId(): number {
-    let maxId = -1;
-    todos.value.forEach((todo) => {
-      if (todo.id > maxId) {
-        maxId = todo.id;
+  return maxId + 1;
+}
+
+function addTodo(defaultTodo: Todo) {
+  defaultTodo.id = findNextId();
+  todos.value.push(defaultTodo);
+}
+
+function deleteTodo(todoId: number) {
+  const indexToRemoveFrom = todos.value.findIndex((obj) => obj.id === todoId);
+  todos.value.splice(indexToRemoveFrom, 1);
+}
+
+function updateTodo(newTodo: Todo, todoId: number) {
+  const indexToUpdateAt = todos.value.findIndex((todo) => todo.id === todoId);
+  const todoToUpdate = todos.value[indexToUpdateAt];
+  if (newTodo.title) {
+    todoToUpdate.title = newTodo.title;
+  } else {
+    todoToUpdate.title = 'Title';
+  }
+  if (newTodo.content) {
+    todoToUpdate.content = newTodo.content;
+  } else {
+    todoToUpdate.content = 'Description';
+  }
+  todoToUpdate.importance = newTodo.importance;
+  todoToUpdate.isEditing = false;
+  todoToUpdate.date = newTodo.date;
+
+  if (isSortingApplied.value) applySortBy(sortByField.value);
+}
+
+function setEditState(todoId: number, value: boolean) {
+  const indexToUpdateAt = todos.value.findIndex((obj) => obj.id === todoId);
+  todos.value[indexToUpdateAt].isEditing = value;
+}
+
+function handleCheckboxClick(todoId: number) {
+  const indexToUpdateAt = todos.value.findIndex((todo) => todo.id === todoId);
+  const todoToMakeFloat = todos.value[indexToUpdateAt];
+  todoToMakeFloat.isChecked = !todoToMakeFloat.isChecked;
+  if (isSortingApplied.value) {
+    return;
+  } else {
+    setTimeout(() => {
+      if (todoToMakeFloat.isChecked) {
+        todos.value.splice(indexToUpdateAt, 1);
+        todos.value.unshift(todoToMakeFloat);
+      } else {
+        todos.value.splice(indexToUpdateAt, 1);
+        todos.value.push(todoToMakeFloat);
       }
-    });
-    return maxId + 1;
+    }, 500);
   }
-  
-  function addTodo(defaultTodo: Todo) {
-    defaultTodo.id = findNextId();
-    todos.value.push(defaultTodo);
-  }
-  
-  function deleteTodo(todoId: number) {
-    const indexToRemoveFrom = todos.value.findIndex((obj) => obj.id === todoId);
-    todos.value.splice(indexToRemoveFrom, 1);
-  }
-  
-  function updateTodo(newTodo: Todo, todoId: number) {
-    const indexToUpdateAt = todos.value.findIndex((todo) => todo.id === todoId);
-    const todoToUpdate = todos.value[indexToUpdateAt];
-    if (newTodo.title) {
-      todoToUpdate.title = newTodo.title;
-    } else {
-      todoToUpdate.title = 'Title';
-    }
-    if (newTodo.content) {
-      todoToUpdate.content = newTodo.content;
-    } else {
-      todoToUpdate.content = 'Description';
-    }
-    todoToUpdate.importance = newTodo.importance;
-    todoToUpdate.isEditing = false;
-    todoToUpdate.date = newTodo.date;
-  
-    if (isSortingApplied.value) applySortBy(sortByField.value);
-  }
-  
-  function setEditState(todoId: number, value: boolean) {
-    const indexToUpdateAt = todos.value.findIndex((obj) => obj.id === todoId);
-    todos.value[indexToUpdateAt].isEditing = value;
-  }
-  
-  function handleCheckboxClick(todoId: number) {
-    const indexToUpdateAt = todos.value.findIndex((todo) => todo.id === todoId);
-    const todoToMakeFloat = todos.value[indexToUpdateAt];
-    todoToMakeFloat.isChecked = !todoToMakeFloat.isChecked;
-    if (isSortingApplied.value) {
-      return;
-    } else {
-      setTimeout(() => {
-        if (todoToMakeFloat.isChecked) {
-          todos.value.splice(indexToUpdateAt, 1);
-          todos.value.unshift(todoToMakeFloat);
-        } else {
-          todos.value.splice(indexToUpdateAt, 1);
-          todos.value.push(todoToMakeFloat);
-        }
-      }, 500);
-    }
-  }
-  
-  function filterTodos(searchInput: string) {
-    searchText.value = searchInput.toLowerCase();
-  }
-  
-  function sortByTitle() {
-    todos.value.sort((a, b) => {
-      const titleA = a.title.toLowerCase();
-      const titleB = b.title.toLowerCase();
-      return sortType.value === 'desc'
-        ? titleB.localeCompare(titleA)
-        : titleA.localeCompare(titleB);
-    });
-  }
-  
-  function compareDateComponents(componentA: string, componentB: string) {
+}
+
+function filterTodos(searchInput: string) {
+  searchText.value = searchInput.toLowerCase();
+}
+
+function sortByTitle() {
+  todos.value.sort((a, b) => {
+    const titleA = a.title.toLowerCase();
+    const titleB = b.title.toLowerCase();
     return sortType.value === 'desc'
-      ? componentB.localeCompare(componentA)
-      : componentA.localeCompare(componentB);
-  }
-  
-  function sortByDate() {
-    todos.value.sort((a, b) => {
-      const dateA = a.date;
-      const dateB = b.date;
-  
-      const [dayA, monthA, yearA] = dateA.split('.');
-      const [dayB, monthB, yearB] = dateB.split('.');
-  
-      const yearComparison = compareDateComponents(yearA, yearB);
-      if (yearComparison !== 0) {
-        return yearComparison;
-      }
-  
-      const monthComparison = compareDateComponents(monthA, monthB);
-      if (monthComparison !== 0) {
-        return monthComparison;
-      }
-  
-      return compareDateComponents(dayA, dayB);
-    });
-  }
-  
-  function sortByPriority() {
-    const priorityValues: Record<string, number> = {
-      High: 3,
-      Medium: 2,
-      Low: 1,
-    };
-  
-    todos.value.sort((a, b) => {
-      const priorityA = priorityValues[a.importance];
-      const priorityB = priorityValues[b.importance];
-      return sortType.value === 'desc'
-        ? priorityB - priorityA
-        : priorityA - priorityB;
-    });
-  }
-  
-  function sortByDescription() {
-    todos.value.sort((a, b) => {
-      const descA = a.content.toLowerCase();
-      const descB = b.content.toLowerCase();
-      return sortType.value === 'desc'
-        ? descB.localeCompare(descA)
-        : descA.localeCompare(descB);
-    });
-  }
-  
-  function stopSort() {
-    isSortingApplied.value = false;
-    filteredTodos.value.forEach((todo) => {
-      setTimeout(() => {
-        if (todo.isChecked) {
-          todo.isChecked = false;
-          handleCheckboxClick(todo.id);
-        }
-      }, 300);
-    });
-  }
-  
-  function applySortBy(field: string) {
-    sortByField.value = field;
-    if (sortByField.value === '') {
-      stopSort();
-      return;
+      ? titleB.localeCompare(titleA)
+      : titleA.localeCompare(titleB);
+  });
+}
+
+function compareDateComponents(componentA: string, componentB: string) {
+  return sortType.value === 'desc'
+    ? componentB.localeCompare(componentA)
+    : componentA.localeCompare(componentB);
+}
+
+function sortByDate() {
+  todos.value.sort((a, b) => {
+    const dateA = a.date;
+    const dateB = b.date;
+
+    const [dayA, monthA, yearA] = dateA.split('.');
+    const [dayB, monthB, yearB] = dateB.split('.');
+
+    const yearComparison = compareDateComponents(yearA, yearB);
+    if (yearComparison !== 0) {
+      return yearComparison;
     }
-    isSortingApplied.value = true;
-    switch (field) {
-      case 'title': {
-        sortByTitle();
-        break;
+
+    const monthComparison = compareDateComponents(monthA, monthB);
+    if (monthComparison !== 0) {
+      return monthComparison;
+    }
+
+    return compareDateComponents(dayA, dayB);
+  });
+}
+
+function sortByPriority() {
+  const priorityValues: Record<string, number> = {
+    High: 3,
+    Medium: 2,
+    Low: 1,
+  };
+
+  todos.value.sort((a, b) => {
+    const priorityA = priorityValues[a.importance];
+    const priorityB = priorityValues[b.importance];
+    return sortType.value === 'desc'
+      ? priorityB - priorityA
+      : priorityA - priorityB;
+  });
+}
+
+function sortByDescription() {
+  todos.value.sort((a, b) => {
+    const descA = a.content.toLowerCase();
+    const descB = b.content.toLowerCase();
+    return sortType.value === 'desc'
+      ? descB.localeCompare(descA)
+      : descA.localeCompare(descB);
+  });
+}
+
+function stopSort() {
+  isSortingApplied.value = false;
+  filteredTodos.value.forEach((todo) => {
+    setTimeout(() => {
+      if (todo.isChecked) {
+        todo.isChecked = false;
+        handleCheckboxClick(todo.id);
       }
-      case 'date': {
-        sortByDate();
-        break;
-      }
-      case 'importance': {
-        sortByPriority();
-        break;
-      }
-      case 'description': {
-        sortByDescription();
-        break;
-      }
+    }, 300);
+  });
+}
+
+function applySortBy(field: string) {
+  sortByField.value = field;
+  if (sortByField.value === '') {
+    stopSort();
+    return;
+  }
+  isSortingApplied.value = true;
+  switch (field) {
+    case 'title': {
+      sortByTitle();
+      break;
+    }
+    case 'date': {
+      sortByDate();
+      break;
+    }
+    case 'importance': {
+      sortByPriority();
+      break;
+    }
+    case 'description': {
+      sortByDescription();
+      break;
     }
   }
-  
-  function changeSortType(newType: string) {
-    sortType.value = newType;
-  
-    applySortBy(sortByField.value);
-  }
-  </script>
-  
+}
+
+function changeSortType(newType: string) {
+  sortType.value = newType;
+
+  applySortBy(sortByField.value);
+}
+</script>
--- client/src/views/SignUpPage.vue
+++ client/src/views/SignUpPage.vue
@@ -1,70 +1,74 @@
 <template>
-  <div class="flex flex-col items-center justify-center">
-    <div class="w-[640px]">
-      <TodoLogin button-name="Log in" path="/login" />
-    </div>
+  <div class="flex flex-col items-center justify-center font-custom">
+    <TodoLogin button-name="Log in" path="/login" />
     <div
-      class="relative mt-20 flex w-screen items-center justify-center bg-white"
+      class="relative mt-2 flex w-72 flex-col items-center justify-center bg-white sm:mt-10 sm:w-screen sm:flex-row"
     >
       <div
-        class="mb-10 mr-60 rounded-xl border-2 bg-neutral-200 bg-opacity-60 p-14 shadow-lg"
+        class="w-64 rounded-xl border-2 bg-neutral-200 bg-opacity-60 p-7 shadow-lg sm:mb-10 sm:mr-60 sm:w-max sm:p-14"
       >
-        <form name="login-form" class="w-[16rem]">
+        <form name="login-form" class="sm:w-[16rem]">
           <div class="mb-3 flex flex-col justify-between">
-            <label for="first-name" class="text-sm font-semibold"
+            <label for="first-name" class="text-xs font-semibold sm:text-sm"
               >First name</label
             >
             <input
               type="text"
               id="first-name"
-              class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+              class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 text-xs text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:p-2.5 sm:text-sm"
               placeholder="Ana"
             />
           </div>
           <div class="mb-3 flex flex-col justify-between">
-            <label for="last-name" class="text-sm font-semibold"
+            <label for="last-name" class="text-xs font-semibold sm:text-sm"
               >Last name</label
             >
             <input
               type="text"
               id="last-name"
-              class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+              class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 text-xs text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:p-2.5 sm:text-sm"
               placeholder="Banana"
             />
           </div>
           <div class="mb-3 flex flex-col justify-between">
-            <label for="username" class="text-sm font-semibold">Username</label>
+            <label for="username" class="text-xs font-semibold sm:text-sm"
+              >Username</label
+            >
             <input
               type="text"
               id="email"
-              class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+              class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 text-xs text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:p-2.5 sm:text-sm"
               placeholder="anabanana"
             />
           </div>
           <div class="mb-3 flex flex-col justify-between">
-            <label for="email" class="text-sm font-semibold">Email</label>
+            <label for="email" class="text-xs font-semibold sm:text-sm"
+              >Email</label
+            >
             <input
               type="email"
               name="email"
               id="email"
-              class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+              class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 text-xs text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:p-2.5 sm:text-sm"
               placeholder="name@company.com"
             />
           </div>
-          <div class="mb-8 flex flex-col justify-between">
-            <label for="password" class="text-sm font-semibold">Password</label>
+          <div class="mb-4 flex flex-col justify-between sm:mb-8">
+            <label for="password" class="text-xs font-semibold sm:text-sm"
+              >Password</label
+            >
             <input
               type="password"
               name="password"
               id="password"
               placeholder="••••••••"
-              class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+              class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 text-xs text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:p-2.5 sm:text-sm"
             />
           </div>
-          <div class="flex justify-end">
+          <div class="flex flex-col">
             <router-link to="/login">
               <button
-                class="rounded-lg bg-teal-400 px-6 py-2 font-semibold text-white"
+                class="sm:text-md w-full rounded-lg bg-teal-400 px-6 py-1.5 text-sm font-semibold text-white sm:py-2"
                 type="submit"
               >
                 Sign up
@@ -73,7 +77,7 @@
           </div>
         </form>
       </div>
-      <PlaceholderIcon class="absolute ml-[20rem]" />
+      <PlaceholderIcon class="w-48 sm:absolute sm:ml-[20rem] sm:mt-0 sm:w-64" />
     </div>
   </div>
 </template>

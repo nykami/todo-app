ba42d50c15c16b75d47a6526b9c0d3b8e7c37e59
nykami <kamilla.nyitrai@yahoo.com>
2023-08-23 09:35:56 +0300

    ITL-9.1 Create user schema

---
+++ server/model/userModel.ts
@@ -0,0 +1,20 @@
+import { Schema, Model, model } from 'mongoose';
+
+export interface IUser {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
+const userSchema: Schema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+export const userModel: Model<IUser> = model<IUser>('User', userSchema);
